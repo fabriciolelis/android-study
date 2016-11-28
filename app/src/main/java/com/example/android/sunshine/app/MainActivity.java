149ff88f05cd3151bc75b753a1133366278db437
Fabricio Lelis <fabricio.melo@virtus.ufcg.edu.br>
2016-11-28 18:22:45 -0300

    initial android studies.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -50,6 +54,8 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        private ArrayAdapter<String> adapter;
+
         public PlaceholderFragment() {
         }
 
@@ -57,6 +63,22 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList<String> forecast = new ArrayList<String>();
+            forecast.add("Today - Sunny - 88/63");
+            forecast.add("Tomorrow - Foggy - 70/46");
+            forecast.add("Weds - Cloudy - 72/63");
+            forecast.add("Thurs - Rainy - 64/51");
+            forecast.add("Fri - Foggy - 70/46");
+            forecast.add("Sat - Sunny - 76/68");
+            forecast.add("Sun - Sunny - 80/68");
+
+            adapter = new ArrayAdapter<String>(getActivity(),
+                    R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecast);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapter);
+
             return rootView;
         }
     }
