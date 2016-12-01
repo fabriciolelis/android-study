2786a86fbf7c5a0320d988015669ab484531daa7
Fabricio Lelis <fabricio.melo@virtus.ufcg.edu.br>
2016-12-01 17:20:03 -0300

    insert refresh button

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.android.sunshine.app;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
